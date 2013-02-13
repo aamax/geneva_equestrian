0e70b875158b92b8cceb77c7d259a8f192da5952
Allen Maxwell <aamax@xmission.com>
2013-02-12 19:44:53 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenevaEquestrian::Application.config.secret_token = '58f1db2b9b25635d3697efa1f82beecbbb864e57541e79f076ec5e86f8935f4bbae40f02dcd2ff9b3d59848518c5f5b39ef4c3556d3f0450a2dd199dd43d6922'
